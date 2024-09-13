63f19ad055850e323427770a15ed09c82ec3c2e5
Eric_Gdf <79082962+Ericgdf@users.noreply.github.com>
2024-09-13 16:19:25 +0200

    Feature/import first component (#3)
    * import first component focntionnel
    
    * fix doublon
    
    * clean package
    
    * clean code

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
-// src/index.ts
+import scoYellowLogo from './ressources/logo/yellow_logo_blue_text.svg';
+import scoWhiteLogo from './ressources/logo/white_logo_white_text.svg';
+import scoLogoOnly from './ressources/logo/logo_only_yellow.svg';
+import './mppStyle.css'
+export { MppButton, ButtonType } from './components/MppButton' ;
+export  {default as MppInputText } from './components/MppInputText/MppInputText';
+export { scoYellowLogo, scoWhiteLogo, scoLogoOnly };
 
-export { MppButton } from './components/MppButton' ;
+
