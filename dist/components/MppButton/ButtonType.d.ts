563f6aa419da4dc96cafe3068305bc41f371087b
jntdev <jnt.marois@gmail.com>
2024-09-17 11:37:25 +0200

    correctif git ignore

---
+++ dist/components/MppButton/ButtonType.d.ts
@@ -0,0 +1,6 @@
+export declare enum ButtonType {
+    primaryLarge = 0,
+    primaryMedium = 1,
+    secondaryLarge = 2,
+    secondaryMedium = 3
+}
---
+++ dist/section/MppTextStyleSection/MppTextStyleSection.d.ts
@@ -0,0 +1,4 @@
+import React from 'react';
+import './mpp_text_style.css';
+declare const MppTextStyle: React.FC;
+export default MppTextStyle;
