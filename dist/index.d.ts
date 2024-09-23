19d629f03f98d90000c225a86f1aa53c3d44db87
Eric_Gdf <ericgodefroy@outlook.fr>
2024-09-23 15:01:02 +0200

    test with easy svg

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,9 +1,7 @@
-import { ReactComponent as ScoYellowLogo } from './ressources/logo/sco_yellow_logo_blue_text.svg';
-import { ReactComponent as ScoWhiteLogo } from './ressources/logo/sco_white_logo_white_text.svg';
-import { ReactComponent as ScoLogoOnly } from './ressources/logo/sco_logo_only_yellow.svg';
-import { ReactComponent as GpBlueLogo } from './ressources/logo/gp_blue_logo_blue_text.svg';
+import { ReactComponent as round } from './ressources/logo/ring.svg';
+import './mppStyle.css';
 export { MppButton, ButtonType } from './components/MppButton';
 export { default as MppInputText } from './components/MppInputText/MppInputText';
 export { ScoColors } from './utils/Mppcolors';
-export { ScoYellowLogo, ScoWhiteLogo, ScoLogoOnly, GpBlueLogo };
+export { round };
 export { default as MppRankingCard } from './components/MppRankingCard/MppRankingCard';
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
-import { ReactComponent as ScoYellowLogo } from './ressources/logo/sco_yellow_logo_blue_text.svg';
-import { ReactComponent as ScoWhiteLogo } from './ressources/logo/sco_white_logo_white_text.svg';
-import { ReactComponent as ScoLogoOnly } from './ressources/logo/sco_logo_only_yellow.svg';
-import { ReactComponent as GpBlueLogo } from './ressources/logo/gp_blue_logo_blue_text.svg';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import React from 'react';
+// import { ReactComponent as ScoYellowLogo } from './ressources/logo/sco_yellow_logo_blue_text.svg';
+// import { ReactComponent as ScoWhiteLogo } from './ressources/logo/sco_white_logo_white_text.svg';
+// import { ReactComponent as ScoLogoOnly } from './ressources/logo/sco_logo_only_yellow.svg';
+// export { ScoYellowLogo, ScoWhiteLogo, ScoLogoOnly, GpBlueLogo };
+// export { MppIcon } from './utils/MppIcons';
+
+import { ReactComponent as round } from './ressources/logo/ring.svg';
+import './mppStyle.css';
 
 export { MppButton, ButtonType } from './components/MppButton';
 export { default as MppInputText } from './components/MppInputText/MppInputText';
 export { ScoColors } from './utils/Mppcolors';
-export { ScoYellowLogo, ScoWhiteLogo, ScoLogoOnly, GpBlueLogo };
+export { round };
+
 export { default as MppRankingCard } from './components/MppRankingCard/MppRankingCard';
