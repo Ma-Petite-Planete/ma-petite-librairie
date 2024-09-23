aa35afc13864ff10e0bd78c1831af120f3b5b5fb
Eric_Gdf <ericgodefroy@outlook.fr>
2024-09-23 16:45:14 +0200

    issue seems fix

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,6 +1,11 @@
+import { ReactComponent as ScoYellowLogo } from './ressources/logo/sco_yellow_logo_blue_text.svg';
+import { ReactComponent as ScoWhiteLogo } from './ressources/logo/sco_white_logo_white_text.svg';
+import { ReactComponent as ScoLogoOnly } from './ressources/logo/sco_logo_only_yellow.svg';
+import { ReactComponent as GpBlueLogo } from './ressources/logo/gp_blue_logo_blue_text.svg';
 import './mppStyle.css';
 export { MppButton, ButtonType } from './components/MppButton';
 export { default as MppInputText } from './components/MppInputText/MppInputText';
 export { ScoColors } from './utils/Mppcolors';
+export { ScoYellowLogo, ScoWhiteLogo, ScoLogoOnly, GpBlueLogo };
 export { default as MppRankingCard } from './components/MppRankingCard/MppRankingCard';
 export { MppIcons } from './utils/MppIcons';
--- src/utils/MppIcons.tsx
+++ src/utils/MppIcons.tsx
@@ -1,17 +1,17 @@
 import { ReactComponent as pen } from '../ressources/icon/pen.svg';
-// import { ReactComponent as logo } from '../ressources/icon/logo_blue.svg';
-// import { ReactComponent as graph } from '../ressources/icon/graph.svg';
-// import { ReactComponent as help } from '../ressources/icon/help.svg';
-// import { ReactComponent as map } from '../ressources/icon/map.svg';
-// import { ReactComponent as ressources } from '../ressources/icon/ressources.svg';
-// import { ReactComponent as trophee } from '../ressources/icon/trophee.svg';
+import { ReactComponent as logo } from '../ressources/icon/logo_blue.svg';
+import { ReactComponent as graph } from '../ressources/icon/graph.svg';
+import { ReactComponent as help } from '../ressources/icon/help.svg';
+import { ReactComponent as map } from '../ressources/icon/map.svg';
+import { ReactComponent as ressources } from '../ressources/icon/ressources.svg';
+import { ReactComponent as trophee } from '../ressources/icon/trophee.svg';
 
 export const MppIcons = {
   pen: pen,
-  // logo: logo,
-  // graph: graph,
-  // help: help,
-  // map: map,
-  // ressources: ressources,
-  // trophee: trophee,
+  logo: logo,
+  graph: graph,
+  help: help,
+  map: map,
+  ressources: ressources,
+  trophee: trophee,
 } as const;
