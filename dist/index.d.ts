bc2c900a5d12b8d6faf1949e0fb98a7aa61a7ef0
Eric_Gdf <ericgodefroy@outlook.fr>
2024-09-23 15:47:11 +0200

    _

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,9 +1,10 @@
-import { ReactComponent as MppRound } from './ressources/logo/ring.svg';
 import { ReactComponent as ScoYellowLogo } from './ressources/logo/sco_yellow_logo_blue_text.svg';
+import { ReactComponent as ScoWhiteLogo } from './ressources/logo/sco_white_logo_white_text.svg';
+import { ReactComponent as ScoLogoOnly } from './ressources/logo/sco_logo_only_yellow.svg';
+import { ReactComponent as GpBlueLogo } from './ressources/logo/gp_blue_logo_blue_text.svg';
 import './mppStyle.css';
 export { MppButton, ButtonType } from './components/MppButton';
 export { default as MppInputText } from './components/MppInputText/MppInputText';
 export { ScoColors } from './utils/Mppcolors';
-export { MppRound, ScoYellowLogo };
-export { MppIcon } from './utils/MppIcons';
+export { ScoYellowLogo, ScoWhiteLogo, ScoLogoOnly, GpBlueLogo };
 export { default as MppRankingCard } from './components/MppRankingCard/MppRankingCard';
