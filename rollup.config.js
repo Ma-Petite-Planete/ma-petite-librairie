143e8646425b798e48e52bfd15be6d5e66943bc4
Eric_Gdf <ericgodefroy@outlook.fr>
2024-09-23 11:55:55 +0200

    fix rollup issue

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,8 @@ import typescript from '@rollup/plugin-typescript';
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import postcss from 'rollup-plugin-postcss';
+import replace from '@rollup/plugin-replace';
+import babel from '@rollup/plugin-babel';
 
 export default {
   input: 'src/index.ts',
@@ -12,9 +14,18 @@ export default {
     exports: 'named',
   },
   plugins: [
+    replace({
+      preventAssignment: true,
+      'process.env.NODE_ENV': JSON.stringify('development'),
+    }),
     svgr({ exportType: 'named', jsxRuntime: 'automatic' }),
     resolve(),
     commonjs(),
+    babel({
+      babelHelpers: 'bundled',
+      exclude: 'node_modules/**',
+      presets: [['@babel/preset-react', { runtime: 'automatic' }]],
+    }),
     typescript(),
     postcss(),
   ],
