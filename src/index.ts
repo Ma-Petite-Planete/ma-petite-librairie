6e35360ffd7cd5c8789bb350318fdd966613134c
Eric_Gdf <ericgodefroy@outlook.fr>
2024-09-23 15:05:23 +0200

    _

--- src/index.ts
+++ src/index.ts
@@ -6,12 +6,12 @@ import React from 'react';
 // export { ScoYellowLogo, ScoWhiteLogo, ScoLogoOnly, GpBlueLogo };
 // export { MppIcon } from './utils/MppIcons';
 
-import { ReactComponent as round } from './ressources/logo/ring.svg';
+import { ReactComponent as Round } from './ressources/logo/ring.svg';
 import './mppStyle.css';
 
 export { MppButton, ButtonType } from './components/MppButton';
 export { default as MppInputText } from './components/MppInputText/MppInputText';
 export { ScoColors } from './utils/Mppcolors';
-export { round };
+export { Round };
 
 export { default as MppRankingCard } from './components/MppRankingCard/MppRankingCard';
