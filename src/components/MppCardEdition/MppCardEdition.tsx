1c85c3eced95d1f30f85da3eefcb5fb189957f23
Mathilde-J <72550752+Mathilde-J@users.noreply.github.com>
2024-10-01 11:09:13 +0200

    wip - ajout couleur

--- src/components/MppCardEdition/MppCardEdition.tsx
+++ src/components/MppCardEdition/MppCardEdition.tsx
@@ -17,7 +17,7 @@ interface MppCardEditionProps {
  * @property {string} textColor - Couleur des textes.
  * @property {string} editionName - Nom de l'édition.
  * @property {string} editionDatesInfos - Nom de l'édition.
- * @property {boolean} editionMessage - Message à afficher sur la droite donnant l'état d'avancement de l'édition.
+ * @property {string} editionMessage - Message à afficher sur la droite donnant l'état d'avancement de l'édition.
  *
  * Composant d'affichage des dates de début et de fin d'édition et, côté scolaire, affichage d'un message en fonction de la date du jour
  *
