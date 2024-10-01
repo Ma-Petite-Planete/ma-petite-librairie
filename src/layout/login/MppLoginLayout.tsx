1147f91435030dcde0a87d9b6866dc29bf561b25
Eric_Gdf <ericgodefroy@outlook.fr>
2024-10-01 14:59:27 +0200

    css desktop + responsive ok

--- src/layout/login/MppLoginLayout.tsx
+++ src/layout/login/MppLoginLayout.tsx
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './mpp_login_layout.css';
 import { MppButton, ButtonType } from '../../components/MppButton';
 import { default as MppInputText } from '../../components/MppInputText/MppInputText';
 import { BoType } from '../../components/BoType';
 import { ReactComponent as ScoYellowLogo } from '../../ressources/logo/sco_yellow_logo_blue_text.svg';
+import scoBackgroundMobile from '../../ressources/background/scoBackgroundMobile.png';
 import scoBackgroundDesktop from '../../ressources/background/scoBackground.png';
 import womanOnComputer from '../../ressources/illustration/woman_on_computer.png';
 
@@ -17,11 +18,27 @@ const ComponentName: React.FC<LoginLayoutProps> = ({
   onPressLoginButon,
 }) => {
   const [companyCode, setCompanyCode] = useState<string>('');
+  const [isMobile, setIsMobile] = useState<boolean>(window.innerWidth <= 896);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 896);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
     <div className="container_login_background">
       <div
-        className={`container_right_side ${boType === BoType.scoBO ? 'login_sco_background' : 'login_gp_background'}`}
-        style={{ backgroundImage: `url(${scoBackgroundDesktop})` }}
+        className={'container_right_side'}
+        style={{
+          backgroundImage: `url(${boType === BoType.scoBO ? (isMobile ? scoBackgroundMobile : scoBackgroundDesktop) : null})`,
+        }}
       >
         {boType === BoType.scoBO ? (
           <ScoYellowLogo className="login_logo" />
@@ -29,26 +46,26 @@ const ComponentName: React.FC<LoginLayoutProps> = ({
 
         <div className="login_welcome_text_container">
           <p className="title_h2 welcome_text">
-            Bienvenue sur l espace client{' '}
+            Bienvenue sur lespace client{' '}
             <span className="title_h1">Mpp Scolaire</span>
           </p>
           <p>
-            La plateforme qui permet aux chef.fe.s d etablissement de de
-            visualiser leurparticipation au challenge
+            La plateforme qui permet aux chef.fe.s détablissement de visualiser
+            leur participation au challenge
           </p>
         </div>
 
         <img
           className="login_illustration"
           src={womanOnComputer}
-          alt="illustarion of woman on computer"
+          alt="illustration of woman on computer"
         />
       </div>
 
       <div className="container_left_side">
         <div className="left_side_content">
           <p className="title_h1">Connexion</p>
-          <p>Rentre le code que l équipe Mpp ta transmis </p>
+          <p>Rentre le code que léquipe Mpp ta transmis </p>
           <div>
             <MppInputText
               placeholder={'Code Client'}
