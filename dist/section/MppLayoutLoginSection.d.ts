413e8a8e1f4ca54f22c2be5b79fd3e486a469e5d
Mathilde <mthldjanssen@gmail.com>
2024-10-24 15:39:40 +0200

    __

--- dist/section/MppLayoutLoginSection.d.ts
+++
@@ -1,3 +0,0 @@
-import React from 'react';
-declare const LayoutLoginSection: React.FC;
-export default LayoutLoginSection;
--- dist/section/MppTextStyleSection/MppTextStyleSection.d.ts
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-import './mpp_text_style.css';
-declare const MppTextStyle: React.FC;
-export default MppTextStyle;
