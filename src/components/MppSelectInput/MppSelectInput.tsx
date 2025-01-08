db3152d06d7a2b62c5894dc4de0283a54b470400
Mathilde <mthldjanssen@gmail.com>
2025-01-08 15:00:49 +0100

    save dropdown visual

--- src/components/MppSelectInput/MppSelectInput.tsx
+++ src/components/MppSelectInput/MppSelectInput.tsx
@@ -8,22 +8,21 @@ interface MppSelectInputProps {
 }
 
 const MppSelectInput: React.FC<MppSelectInputProps> = () => {
+  const [selectedOption, setSelectedOption] = React.useState('');
   return (
-    <div>
-      <div className="custom-select">
-        <button className="select-button">
-          <span className="selected-value text_body">DropDown rempli</span>
-          <span className="arrow"></span>
-        </button>
-        <ul className="select-dropdown hidden text_body">
+    <div className="custom-select">
+      <button className="select-button text_body">
+        <span>DropDown rempli</span>
+        <span className="arrow"></span>
+      </button>
+      {/* <ul className="select-dropdown hidden text_body">
           <li>GitHub</li>
           <li>Instagram</li>
           <li>Facebook</li>
           <li>LinkedIn</li>
           <li>Twitter</li>
           <li>Reddit</li>
-        </ul>
-      </div>
+        </ul> */}
     </div>
   );
 };
