f9c734333050d2518ceb18086ea114618af90d5f
Mathilde <mthldjanssen@gmail.com>
2025-01-08 15:36:19 +0100

    change value

--- src/components/MppSelectInput/MppSelectInput.tsx
+++ src/components/MppSelectInput/MppSelectInput.tsx
@@ -5,24 +5,44 @@ interface MppSelectInputProps {
   options: Array<{ value: string; prefixIcon?: string }>;
   onChange: (value: string) => void;
   value: string;
+  placeholder?: string;
 }
 
-const MppSelectInput: React.FC<MppSelectInputProps> = () => {
-  const [selectedOption, setSelectedOption] = React.useState('');
+const MppSelectInput: React.FC<MppSelectInputProps> = ({
+  placeholder,
+  onChange,
+  options,
+}) => {
+  const [selectedOption, setSelectedOption] = React.useState(null);
+  const [isDropdownVisible, setIsDropdownVisible] = React.useState(false);
+
   return (
     <div className="custom-select">
-      <button className="select-button text_body">
-        <span>DropDown rempli</span>
-        <span className="arrow"></span>
+      <button
+        onClick={() => setIsDropdownVisible(!isDropdownVisible)}
+        className={`select-button text_body ${isDropdownVisible ? 'open' : ''}`}
+      >
+        <span>{selectedOption ?? placeholder ?? ''}</span>
+        <span
+          className={`${isDropdownVisible ? 'arrow_open' : 'arrow'}`}
+        ></span>
       </button>
-      {/* <ul className="select-dropdown hidden text_body">
-          <li>GitHub</li>
-          <li>Instagram</li>
-          <li>Facebook</li>
-          <li>LinkedIn</li>
-          <li>Twitter</li>
-          <li>Reddit</li>
-        </ul> */}
+      <ul className={`select-dropdown ${isDropdownVisible ? '' : 'hidden'}`}>
+        {options.map((option, index) => (
+          <li
+            key={index}
+            onClick={() => {
+              console.log('option', option.value);
+              setSelectedOption(option.value);
+              setIsDropdownVisible(false);
+              onChange(option.value);
+            }}
+          >
+            {option.prefixIcon && <img src={option.prefixIcon} alt="" />}
+            {option.value}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
