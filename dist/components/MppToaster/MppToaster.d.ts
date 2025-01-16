ee76a1059cabd9180dd3303e219677eeffae2003
Mathilde <mthldjanssen@gmail.com>
2025-01-16 12:01:48 +0100

    dist

--- dist/components/MppToaster/MppToaster.d.ts
+++ dist/components/MppToaster/MppToaster.d.ts
@@ -1,21 +1,38 @@
 import React from 'react';
 import './mpp_toaster.css';
+export declare enum MessageType {
+    error = 0,
+    succes = 1
+}
+export declare enum AnimationDirection {
+    from_bottom = "toaster_message_container--bottom",
+    from_top = "toaster_message_container--top"
+}
 interface MppToasterProps {
-    successMessage?: string;
-    errorMessage?: string;
+    message: string;
+    displayToast: boolean;
+    messageType: MessageType;
+    animationDirection: AnimationDirection;
 }
 /**
- * Le composant MppToaster affiche des messages de succès et d'erreur.
+ * Le composant MppToaster rend un message de notification (toast) avec des styles et animations personnalisables.
  *
- * @composant
- * @param {Object} props - L'objet des propriétés.
- * @param {string} props.successMessage - Le message de succès à afficher.
- * @param {string} props.errorMessage - Le message d'erreur à afficher.
+ * @component
+ * @param {MppToasterProps} props - Les propriétés du composant MppToaster.
+ * @param {string} props.message - Le message à afficher dans le toast.
+ * @param {boolean} props.displayToast - Indicateur pour afficher ou masquer le toast.
+ * @param {MessageType} props.messageType - Le type de message (erreur ou succès).
+ * @param {AnimationDirection} props.animationDirection - La direction de l'animation du toast.
  *
- * @exemple
- * <MppToaster successMessage="Opération réussie !" errorMessage="Échec de l'opération !" />
+ * @returns {JSX.Element} Le composant MppToaster rendu.
  *
- * @returns {JSX.Element} Le composant rendu.
+ * @example
+ * <MppToaster
+ *   message="Opération réussie"
+ *   displayToast={true}
+ *   messageType={MessageType.succes}
+ *   animationDirection={AnimationDirection.from_bottom}
+ * />
  */
 declare const MppToaster: React.FC<MppToasterProps>;
 export default MppToaster;
