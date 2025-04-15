b5f167e61e0962f90a34584cc1adca62789fcfc1
Eric_Gdf <ericgodefroy@outlook.fr>
2025-04-15 14:15:16 +0200

    fix last change linear bar

--- src/components/MppLinearProgressBar/MppLinearProgressBar.tsx
+++ src/components/MppLinearProgressBar/MppLinearProgressBar.tsx
@@ -46,20 +46,26 @@ export const MppLinearProgressBar: React.FC<LinearProgressBarProps> = ({
   value,
   colorStyle,
 }) => {
+  const finishPercentage = Math.round((value / maxValue) * 100);
+
   return (
     <div className={`linear_progress_bar_container ${colorStyle}`}>
       <div className="linear_progress_bar--background_value">
         <div className="progress_bar background_value--indicator">
           <div
             className="linear_progress_bar--main_value"
-            style={{ width: `${Math.round((value / maxValue) * 100)}%` }}
+            style={{ width: `${finishPercentage}%` }}
           >
             <div className="progress_bar main_value--indicator"></div>
             <p className="main_value--value">{Math.round(value)}</p>
           </div>
         </div>
       </div>
-      <p className="background_value--max_value">{maxValue}</p>
+      <p
+        className={`background_value--max_value ${finishPercentage > 80 ? 'end_line_number' : ''}`}
+      >
+        {maxValue}
+      </p>
     </div>
   );
 };
