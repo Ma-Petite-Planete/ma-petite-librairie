967053c690b018cd17f922391427f5b3e0c740f7
jntdev <jnt.marois@gmail.com>
2025-07-25 10:49:20 +0200

    _

--- dist/index.js
+++ dist/index.js
@@ -1822,6 +1822,9 @@ const MppToaster = ({ message, displayToast, messageType, animationDirection, on
  */
 const MppToggleButton = ({ id, value, onChange, disabled = false, }) => {
     const [toggleValue, setToggleValue] = useState(value);
+    useEffect(() => {
+        setToggleValue(value);
+    }, [value]);
     return (React__default.createElement("div", { className: 'toggle_button_container' },
         React__default.createElement("label", { htmlFor: id, className: `toggle_button ${toggleValue ? 'checked' : ''} ${disabled ? 'disabled_container' : ''}` },
             React__default.createElement("input", { onChange: () => {
